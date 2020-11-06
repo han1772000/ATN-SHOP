61aaf91bfdabf1e26034780603213a0596cc186a
unknown <trinhngocphuc8@gmail.com>
2020-11-06 15:24:39 +0700

    update source

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb://localhost:27017/ATNShop',
+        dbname : "ATNShop",
+        dbusername : "",
+        dbpassword : "",
+    },
+
+    clouddb: {
+        urldb : 'mongodb://localhost:27017/ATNShop',
+        dbname : "ATNShop",
+        dbusername : "",
+        dbpassword : "",
+    }
+
+};
